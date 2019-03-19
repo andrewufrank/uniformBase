c10ef02037b62d0c3e16412d70d928c1faf981c9
andrew u frank <frank@santafe.local>
2019-03-19 17:49:19 +0100

    add NiceStrings instance

--- uniform-fileio/Uniform/Filenames.hs
+++ uniform-fileio/Uniform/Filenames.hs
@@ -16,46 +16,58 @@
 {-# LANGUAGE ScopedTypeVariables   #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
-{-# LANGUAGE OverloadedStrings
-        , DeriveGeneric
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE   DeriveGeneric    #-}
+{-# LANGUAGE   UndecidableInstances    #-}
         -- , DeriveAnyClass
-         #-}
 -- {-# OPTIONS_GHC -fno-warn-missing-methods #-}
 
-module Uniform.Filenames  (
-         module Uniform.Filenames
-         , module Uniform.Error
-         , Abs, Rel, File, Dir, Path, toFilePath 
-             ) where
-import qualified Data.List.Split          as Sp -- hiding ((</>), (<.>))
-import qualified Path  as Path -- for Generics
-import Path (Path(..), toFilePath)
+module Uniform.Filenames
+    ( module Uniform.Filenames
+    , module Uniform.Error
+    , Abs
+    , Rel
+    , File
+    , Dir
+    , Path
+    , toFilePath
+    )
+where
+import qualified Data.List.Split               as Sp -- hiding ((</>), (<.>))
+import qualified Path                          as Path -- for Generics
+import           Path                           ( Path(..)
+                                                , toFilePath
+                                                )
 
-import           Path (Abs, Rel, File, Dir)
-import qualified Path.IO as PathIO
-import qualified System.FilePath       as S
+import           Path                           ( Abs
+                                                , Rel
+                                                , File
+                                                , Dir
+                                                )
+import qualified Path.IO                       as PathIO
+import qualified System.FilePath               as S
 
-import qualified System.FilePath.Posix       as S -- prefered
+import qualified System.FilePath.Posix         as S -- prefered
 import           Uniform.Error -- prefered
 import           Uniform.Zero
 --import  qualified         Filesystem.Path       as F -- prefered
 -- not usable, has a different definition of FilePath
-import Uniform.PathShowCase  -- read and show for Path
+import           Uniform.PathShowCase  -- read and show for Path
 
-homeDir =  makeAbsDir "/home/frank/":: Path Abs Dir
-homeDir2 ::ErrIO (Path Abs Dir)
-homeDir2 = callIO $ PathIO.getHomeDir  ::ErrIO (Path Abs Dir)
+homeDir = makeAbsDir "/home/frank/" :: Path Abs Dir
+homeDir2 :: ErrIO (Path Abs Dir)
+homeDir2 = callIO $ PathIO.getHomeDir :: ErrIO (Path Abs Dir)
 -- replace homeDir with homeDir2 - is user independent but requires IO
-currentDir::ErrIO (Path Abs Dir)
+currentDir :: ErrIO (Path Abs Dir)
 currentDir = callIO $ PathIO.getCurrentDir
 
 setCurrentDir :: Path Abs Dir -> ErrIO ()
 setCurrentDir path = PathIO.setCurrentDir (unPath path)
 
-stripProperPrefix' ::  Path b Dir -> Path b t -> ErrIO (Path Rel t)
+stripProperPrefix' :: Path b Dir -> Path b t -> ErrIO (Path Rel t)
 stripProperPrefix' dir fn = Path.stripProperPrefix (unPath dir) (unPath fn)
 
-instance {-# OVERLAPPABLE #-} Show (Path a b) where  
+instance {-# OVERLAPPABLE #-} Show (Path a b) where
     show a = error "Show (Path a b) - the generic instance should not be used"
 
 -- newtype Path b t = Path (Path.Path b t)
@@ -78,7 +90,7 @@ instance {-# OVERLAPPABLE #-} Show (Path a b) where
 -- instance NiceStrings (Path a b) where
 --     shownice = s2t . Path.toFile unPath
 
-unPath   = id
+unPath = id
 -- toFilePath = Path.toFile unPath
 
 
@@ -90,32 +102,34 @@ makeRelDir :: FilePath -> Path Rel Dir
 makeAbsFile :: FilePath -> Path Abs File
 makeAbsDir :: FilePath -> Path Abs Dir
 
-makeRelFile fn =  fromJustNote ("makeRelFile " ++ fn)
-                        $ Path.parseRelFile fn
-makeRelDir fn =  fromJustNote ("makeRelDir " ++ fn)
-                        $ Path.parseRelDir fn
-makeAbsFile fn =  fromJustNote ("makeAbsFile " ++ fn)
-                        $ Path.parseAbsFile fn
-makeAbsDir fn =  fromJustNote ("makeAbsDir " ++ fn)
-                        $ Path.parseAbsDir fn
+makeRelFile fn = fromJustNote ("makeRelFile " ++ fn) $ Path.parseRelFile fn
+makeRelDir fn = fromJustNote ("makeRelDir " ++ fn) $ Path.parseRelDir fn
+makeAbsFile fn = fromJustNote ("makeAbsFile " ++ fn) $ Path.parseAbsFile fn
+makeAbsDir fn = fromJustNote ("makeAbsDir " ++ fn) $ Path.parseAbsDir fn
 
 toShortFilePath :: Path df ar -> FilePath
 ---- ^ get the filepath, but without the trailing separator
 --    , necessary for systemcalls
 toShortFilePath = S.dropTrailingPathSeparator . toFilePath
 
-instance Zeros (Path Abs Dir) where zero = makeAbsDir "/"
-instance Zeros (Path Abs File) where zero = makeAbsFile "/zero"
-instance Zeros (Path Rel Dir) where zero = makeRelDir "./"
-instance Zeros (Path Rel File) where zero = makeRelFile "zero"
+instance Zeros (Path Abs Dir) where
+    zero = makeAbsDir "/"
+instance Zeros (Path Abs File) where
+    zero = makeAbsFile "/zero"
+instance Zeros (Path Rel Dir) where
+    zero = makeRelDir "./"
+instance Zeros (Path Rel File) where
+    zero = makeRelFile "zero"
 instance IsString (Path Abs File) where
-    fromString = read  
+    fromString = read
 instance IsString (Path Abs Dir) where
     fromString = read
 instance IsString (Path Rel File) where
     fromString = read
 instance IsString (Path Rel Dir) where
     fromString = read
+instance  NiceStrings (Path a b) where
+    shownice = s2t . toFilePath
 
 -- instance Read (Path Abs Dir) where
 --         readsPrec i r =   maybe []  (\res -> [(Path res, rem1)] )
@@ -161,7 +175,7 @@ class Filenames5 dir fil res where
     stripPrefix :: dir -> fil ->  Maybe res
     -- ^ strip the
 instance Filenames5 (Path b Dir) (Path b t)  (Path Rel t) where
-    stripPrefix d f =  Path.stripProperPrefix (unPath d) (unPath f)
+    stripPrefix d f = Path.stripProperPrefix (unPath d) (unPath f)
 
 class Filenames4 fp file  where
     type FileResultT4 fp file
@@ -184,57 +198,55 @@ instance Filenames FilePath FilePath where
     getFileName = snd . S.splitFileName
 instance Filenames3 FilePath FilePath  where
     type FileResultT FilePath FilePath = FilePath
-    addFileName  = S.combine
+    addFileName = S.combine
 
 instance Filenames (Path ar File) (Path Rel File) where
-    getFileName =  Path.filename . unPath
+    getFileName = Path.filename . unPath
 
 instance Filenames3 (Path b Dir) FilePath  where
     type FileResultT (Path b Dir) FilePath = (Path b File)
-    addFileName p  d = if null' d
-                    then error ("addFileName with empty file" ++ d)
-                    else (Path.</>) (unPath p) (unPath d2)
-        where
-            d2 = makeRelFile d :: Path Rel File
+    addFileName p d = if null' d
+        then error ("addFileName with empty file" ++ d)
+        else (Path.</>) (unPath p) (unPath d2)
+        where d2 = makeRelFile d :: Path Rel File
 
 
 instance Filenames4 FilePath FilePath  where
     type FileResultT4 FilePath FilePath = FilePath
-    addDir p  d =  if null' d then p else (p </>) d
+    addDir p d = if null' d then p else (p </>) d
 
 instance Filenames4 (Path b Dir) FilePath  where
     type FileResultT4 (Path b Dir) FilePath = (Path b Dir)
-    addDir p  d =  if null' d then (unPath p)
-                                    else (Path.</>) (unPath p) (unPath d2)
-        where
-            d2 = makeRelDir d :: Path Rel Dir
+    addDir p d = if null' d
+        then (unPath p)
+        else (Path.</>) (unPath p) (unPath d2)
+        where d2 = makeRelDir d :: Path Rel Dir
 
 instance Filenames4 (Path b Dir) (Path Rel t)  where
     type FileResultT4 (Path b Dir) (Path Rel t) = (Path b t)
-    addDir p  d =  (Path.</>) (unPath p) (unPath d)
+    addDir p d = (Path.</>) (unPath p) (unPath d)
 
 instance Filenames3 (Path b Dir) (Path Rel t)  where
     type FileResultT (Path b Dir) (Path Rel t) = (Path b t)
-    addFileName p  d =   (Path.</>) (unPath p) (unPath d)
+    addFileName p d = (Path.</>) (unPath p) (unPath d)
 
 instance Filenames1 (Path ar File)   where
-    getNakedFileName =   getNakedFileName . toFilePath
+    getNakedFileName      = getNakedFileName . toFilePath
     getImmediateParentDir = getImmediateParentDir . toFilePath
-    getParentDir =  getParentDir . toFilePath
-    getNakedDir = error
-        "getNakedDir for Filenamse1 Path ar File) not existing"
+    getParentDir          = getParentDir . toFilePath
+    getNakedDir = error "getNakedDir for Filenamse1 Path ar File) not existing"
 
 instance Filenames1 (Path ar Dir) where
-    getNakedFileName = error "getNakedFileName not from Dir"
+    getNakedFileName      = error "getNakedFileName not from Dir"
     getImmediateParentDir = getImmediateParentDir . toFilePath
-    getParentDir =  getParentDir . toFilePath
-    getNakedDir = getNakedDir . toFilePath
+    getParentDir          = getParentDir . toFilePath
+    getNakedDir           = getNakedDir . toFilePath
 
 instance Filenames1 FilePath   where
-    getNakedFileName =   removeExtension . getFileName
+    getNakedFileName      = removeExtension . getFileName
     getImmediateParentDir = (!! 1) . reverse . S.splitDirectories
-    getParentDir = S.takeDirectory
-    getNakedDir = (!! 0) . reverse . S.splitDirectories
+    getParentDir          = S.takeDirectory
+    getNakedDir           = (!! 0) . reverse . S.splitDirectories
 
 
 class (Eq (ExtensionType fp)) => Extensions fp where
@@ -261,22 +273,20 @@ instance Extensions FilePath  where
     type ExtensionType FilePath = FilePath
 
     getExtension = removeChar '.' . snd . S.splitExtension
-    addExtension e fp =  fp S.<.> e
-    removeExtension  = fst . S.splitExtension
-    setExtension e  = addExtension e . removeExtension
+    addExtension e fp = fp S.<.> e
+    removeExtension = fst . S.splitExtension
+    setExtension e = addExtension e . removeExtension
 --    hasExtension e = (e ==) . getExtension
 
 instance Extensions (Path ar File) where
     type ExtensionType (Path ar File) = Extension
 
-    getExtension f = Extension  e -- . removeChar '.'
-        where e =  headNote "werwqerqw"
-                    . Path.fileExtension $ f :: String 
+    getExtension f = Extension e -- . removeChar '.'
+        where e = headNote "werwqerqw" . Path.fileExtension $ f :: String
 
-    setExtension e f =  fromJustNote "setExtension"
-                    $ Path.setFileExtension
-                    (unExtension e) (unPath f)
-    addExtension   =  setExtension
-    removeExtension   =  setExtension (Extension "")
+    setExtension e f = fromJustNote "setExtension"
+        $ Path.setFileExtension (unExtension e) (unPath f)
+    addExtension    = setExtension
+    removeExtension = setExtension (Extension "")
 --    hasExtension e f = (e==). getExtension
 
