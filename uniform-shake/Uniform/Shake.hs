5eb38679e52ee26f07c1f87528d17fae3ec86fe4
andrew u frank <frank@santafe.local>
2019-03-21 11:24:19 +0100

    moved all my code out of syd's path module

--- uniform-shake/Uniform/Shake.hs
+++ uniform-shake/Uniform/Shake.hs
@@ -11,13 +11,21 @@
 -- {-# OPTIONS -Wall #-}
 --{-# OPTIONS -fno-warn-missing-signatures #-}
 
-module Uniform.Shake (runErr2action     
+module Uniform.Shake (runErr2action, liftErrIO  
+        , module Uniform.Shake
         , module Uniform.Shake.Path
         , takeBaseName, splitPath 
-        , Action)
-      where
+        , Action
+        , module Uniform.FileIO
+        , module Uniform.Strings
+    --     , module Path
+        -- , module Path.IO
+        , module Development.Shake.FilePath
+        , module Development.Shake
+        , shakeArgs
+        )      where
 
-import Development.Shake (Action)
+import Development.Shake -- (Action, FilePattern, getDirectoryFiles)
 import Development.Shake.FilePath (takeBaseName, splitPath
                         )
         -- (getDirectoryFiles, Action
@@ -25,12 +33,71 @@ import Development.Shake.FilePath (takeBaseName, splitPath
 -- import Uniform.FileIO (makeRelFile)
 
 -- import qualified Path  
--- import  Path  (Path(..), File, Dir, Abs, Rel, toFilePath)
+import  Path hiding ((</>)) 
+                -- (Path(..), File, Dir, Abs, Rel, toFilePath)
 -- import qualified Path.IO
 import Uniform.Error
 import Uniform.Shake.Path
+import Uniform.FileIO 
+import Uniform.Strings hiding ((</>), (<.>))
 
+($-<.>) :: Text -> Path a File -> Path a File
+($-<.>) = replaceExtension'
+
+replaceExtension' :: Text -> Path a File -> Path a File
+-- a flipped version of -<.> 
+replaceExtension' newext filep =
+    setExtension (makeExtension . t2s $ newext) filep
+    -- if isRelative filen 
+    --     then makeRelFile resn 
+    --     else makeAbsFile resn
+    --     where 
+    --             filen = toFilePath filep 
+    --             resn = replaceExtension (t2s newext) filen 
+
+getDirectoryFilesP :: Path Abs Dir -> [FilePattern] -> Action [Path Rel File]
+getDirectoryFilesP d p = do
+    res :: [FilePath] <- getDirectoryFiles (toFilePath d) p
+    return $ map makeRelFile res
+
+
+class Path2nd  a c where
+    stripProperPrefixP :: Path a b -> Path a c -> Path Rel c
+    makeRelativeP  :: Path a Dir -> Path a c -> Path Rel c
+    makeRelativeP = stripProperPrefixP
+    -- ^ strip the first (the prefix) from the second and returns remainder 
+    -- throws error when not prefix or not proper file path 
+    replaceDirectoryP :: Path a Dir -> Path a Dir -> Path a c  -> Path a c
+    -- ^ strip the first (the prefix) and add the second to the third 
+    
+instance Path2nd  a File where
+    stripProperPrefixP a b = fromJustNote
+        ( t2s
+        . unwords'
+        $ ["Path2nd Dir - not a prefix", showT a, "for", showT b]
+        )
+        (fmap makeRelFile ab)
+        where ab = stripPrefix' (toFilePath a) (toFilePath b) :: Maybe FilePath
+
+    replaceDirectoryP pref newpref old = newpref </> rem1 
+        where rem1 = stripProperPrefixP pref old
+
+
+instance Path2nd  a Dir where
+    stripProperPrefixP a b = fromJustNote
+        ( t2s
+        . unwords'
+        $ ["Path2nd Dir - not a prefix", showT a, "for", showT b]
+        )
+        (fmap makeRelDir ab)
+        where ab = stripPrefix' (toFilePath a) (toFilePath b) :: Maybe FilePath
+
+    replaceDirectoryP pref newpref old = newpref </> rem1 
+        where rem1 = stripProperPrefixP pref old
 -- instance Exception [Text] 
+
+liftErrIO :: ErrIO a -> Action a
+liftErrIO = runErr2action 
 
 runErr2action :: ErrIO a -> Action a
 runErr2action op = liftIO $ do
--- uniform-shake/Uniform/Shake/Path.hs
+++ uniform-shake/Uniform/Shake/Path.hs
@@ -13,46 +13,21 @@
 --{-# OPTIONS -fno-warn-missing-signatures #-}
 
 module Uniform.Shake.Path
-    ( getHashedShakeVersionP
-    , getDirectoryFilesP
-    , replaceExtension
-    , stripProperPrefix   -- import because runs in IO (' runs in ErrIO)
-    , stripProperPrefixP
-    , needP
-    , wantP
-    , ($%>)
-    , ($&%>)
-    -- , ($?==)  -- not relevant, needP return toFilePath in matching
-    , orderOnlyP
-    , cmd
-    , liftIO
-    , CmdOption(..)
-    , (?>)
-    , (?==)
-    , phony
-    , want
-    , (</>)
-    , (<.>)
-    , module Uniform.FileIO
-    , module Uniform.Strings
---     , module Path
-    -- , module Path.IO
-    , module Development.Shake.FilePath
-    , module Development.Shake
-    )
-where
+    where
+
+
 
 import           Development.Shake
-import           Development.Shake.FilePath     ( isAbsolute
-                                                , isRelative
-                        -- , isFile, isDir
-                                                )
+-- import           Development.Shake.FilePath     ( isAbsolute
+--                                                 , isRelative
+--                         -- , isFile, isDir
+--                                                 )
         -- (getDirectoryFiles, Action
         --     , Rules, FilePattern)
-import           Uniform.FileIO
-import           Uniform.Strings         hiding ( (</>)
-                                                , (<.>)
-                                                ) -- (Text, t2s)
+-- import           Uniform.FileIO
+-- import           Uniform.Strings         hiding ( (</>)
+--                                                 , (<.>)
+--                                                 ) -- (Text, t2s)
 -- import qualified Path  
 import           Path                           ( Path
                                                 , File
@@ -60,52 +35,13 @@ import           Path                           ( Path
                                                 , Abs
                                                 , Rel
                                                 , toFilePath
-                                                , stripProperPrefix
+                                                -- , stripProperPrefix
                                                 )
--- get </> and <.> from here
 
--- import qualified Path.IO
-
-replaceExtension :: Text -> Path a File -> Path a File
--- a flipped version of -<.> 
-replaceExtension newext filep =
-    setExtension (makeExtension . t2s $ newext) filep
-    -- if isRelative filen 
-    --     then makeRelFile resn 
-    --     else makeAbsFile resn
-    --     where 
-    --             filen = toFilePath filep 
-    --             resn = replaceExtension (t2s newext) filen 
-
-getDirectoryFilesP :: Path Abs Dir -> [FilePattern] -> Action [Path Rel File]
-getDirectoryFilesP d p = do
-    res :: [FilePath] <- getDirectoryFiles (toFilePath d) p
-    return $ map makeRelFile res
 
 getHashedShakeVersionP :: [Path r File] -> IO String
 getHashedShakeVersionP = getHashedShakeVersion . map toFilePath
-
-class Path2nd  a c where
-    stripProperPrefixP :: Path a b -> Path a c -> Path Rel c
-    -- ^ strip the first (the prefix) from the second and returns remainder 
-    -- throws error when not prefix or not proper file path 
-instance Path2nd  a File where
-    stripProperPrefixP a b = fromJustNote
-        ( t2s
-        . unwords'
-        $ ["Path2nd Dir - not a prefix", showT a, "for", showT b]
-        )
-        (fmap makeRelFile ab)
-        where ab = stripPrefix' (toFilePath a) (toFilePath b) :: Maybe FilePath
-instance Path2nd  a Dir where
-    stripProperPrefixP a b = fromJustNote
-        ( t2s
-        . unwords'
-        $ ["Path2nd Dir - not a prefix", showT a, "for", showT b]
-        )
-        (fmap makeRelDir ab)
-        where ab = stripPrefix' (toFilePath a) (toFilePath b) :: Maybe FilePath
-
+                                                
 needP :: [Path r File] -> Action ()
 needP = need . map toFilePath
 
@@ -115,18 +51,10 @@ wantP = want . map toFilePath
 ($%>) :: Path r File -> Action () -> Rules ()
 p $%> a = toFilePath p %> const a
 
--- makeFile :: FilePath -> Path r File 
--- would need a phantom type parameter :: Path r File -> FilePath -> Path r File 
--- makeFile a = if isAbsolute a   
---                         then (makeAbsFile a :: Path Abs File)
---                         else (makeRelFile a :: Path Rel File)
 
 ($&%>) :: [Path r File] -> Action () -> Rules ()
 ps $&%> a = map toFilePath ps &%> const a
 
--- ($?==) :: FilePattern -> Path r File -> Bool
--- pt $?== a = pt ?== (toFilePath a)
--- not usable, the need (even when using needP) gives always a filepath 
 
 orderOnlyP :: [Path r File] -> Action ()
 orderOnlyP = orderOnly . map toFilePath
