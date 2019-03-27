d1eca6ba451e1725107802d5b8f8f1aaf2776115
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2019-03-27 22:52:38 +0100

    example main compiles

--- uniform-cmdLineArgs/src/CmdLineArgsExample.hs
+++ uniform-cmdLineArgs/src/CmdLineArgsExample.hs
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------
 --
--- Module      :   top tests for layout
+-- Module      :   an example for a command line argument setup 
 -----------------------------------------------------------------------------
 {-# OPTIONS_GHC -F -pgmF htfpp #-}
 
@@ -22,60 +22,78 @@ module Main     where
 
 
 import Test.Framework
-import {-@ HTF_TESTS @-} Uniform.Error
+
 import Uniform.Strings
 
+-- | the command line arguments raw
+data LitArgs = LitArgs
+  { argSwitch1 :: Bool -- ^ l - a swtich
+  , argFlag1 :: String -- ^ b - a string
+  , argInFile1  :: Maybe String  -- ^ g - an otpional filename
+   } deriving (Show)
+
+-- | the arguments in the program usable format
+data Inputs = Inputs 
+        { inFile1 :: Path Abs File
+        , flag1 :: Text
+        , switch1 :: Bool
+                    } deriving (Show, Read, Eq)
+
 main = do
-    putIOwords ["HTF errorTest.hs:\n posTest"]
-    r <- htfMainWithArgs ["--quiet"] htf_importedTests
-    putIOwords ["HTF end errorTest.hs:\n posTest", showT r]
-    r2 <- errorTest
-    putIOwords [" errorTest2\n  ", showT r2]
+    putIOwords ["example for Command Line Argument processing"]
+    r <- parseArgs2input
+    putIOwords ["inputs on command line:", showT r]
     return r
 
-errorTest ::   IO Bool
-errorTest = do
-    r <- runErr errorTest2
-    v1 <- case r of
-        Left msg -> do
-                putIOwords  ["errorTest returned Left :", msg]
-                return False
-        Right v -> return v
-    return v1
+parseArgs2input :: Bool -> Text -> Text -> Text ->  ErrIO Inputs
+-- getting cmd line arguments, with a default value for the file1
+-- the two text arguments are used in the cmd arg parse
+parseArgs2input debugFlag filenameDefault t1 t2  = do
+    args1 <- getArgsParsed t1 t2
+    putIOwords ["parseArgs2input: args found", showT args1]
 
+    --    let server = selectServer args :: ServerFlag
+    homeDir :: Path Abs Dir <- homeDir2
+    let inFile1 = argFile1 $ args1 
 
+    let filename1 = if null inFile1   
+        then addFileName homeDir (makeRelFile filenameDefault)
+        else addFileName homeDir (makeRelFile inFile1)
+                                    :: Path Abs File
 
---test2 :: ErrIO Bool
---test2 = do
---    test2catch False
---    return False
---
---  `catchError` \s -> return True
+    let inputs1 = Inputs { inFile1 = filename1
+                         , flag1 = argFlag1
+                         , switch1 = argSwitch1} 
+    
+    putIOwords ["parseArgs2input:  inputs ", showT inputs1]
+return inp
 
-errorTest2 ::   ErrIO Bool
-errorTest2 = do
-    c1 :: Bool <- mustFailM "test throw" $ (throwError "error1")
-    c2 :: Bool <- mustSucceedM "test return ok" $ (return ())
-    c3 <- mustSucceedM "test catch ok" $ test2catch True
-    c4 <- mustSucceedM "test catch ok" $ test2catch False
-    let res = and [c1, c2, c3, c4]
-    return res
+cmdArgs :: Parser LitArgs
+cmdArgs = LitArgs
+     <$> switch
+          ( long "swtich1" <>
+            short 'l' <>
+            value True
+            help "switch (default True) "
+     <*> strOption
+          ( long "flag1" <>
+            short 'b' <>
+            value "Medium" <>
+            metavar "metavar flag needed." <>
+            help "flag (default Medium)" )
+     <*> strOption
+          ( long "file1 (optional)" <>
+            short 'g' <>
+            metavar "File1" <>
+            value "" <>
+            help "filename (default homeDir)" )
 
-
-test2catch :: Bool -> ErrIO ()
-test2catch b =
-    if b then return ()
-        else throwError "test2catch message"
-  `catchError` (\e -> do
-        if not b then putIOwords ["ok - error thrown and caught:", e ]
-            else throwError "test2catch - error caught when not thrown"
-        return ()
-        )
-
-
---instance (Show a) => Strings (ErrOrVal a) where
---    toString (Left msg) = msg
---    toString (Right r) = show r
-
-
-
+getArgsParsed :: Text -> Text -> ErrIO LitArgs
+getArgsParsed t1 t2 = do
+        args <- callIO $ execParser opts
+        return args
+    where
+        opts = info (helper <*> cmdArgs)
+            ( fullDesc
+            <> (progDesc . t2s $ t1)
+            <> (header . t2s $ t2 ))
