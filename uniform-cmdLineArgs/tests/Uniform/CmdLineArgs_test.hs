a8714f56a5355273d3e7235a2827fb69c839b9be
andrew u frank <frank@napoli.gerastree.at>
2019-03-27 22:04:39 +0100

    add a nwe uniform-cmdLineArgs

---
+++ uniform-cmdLineArgs/tests/Uniform/CmdLineArgs_test.hs
@@ -0,0 +1,31 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Uniform.CmdLineArgs_test
+--
+-- | a miniaml set of  
+-----------------------------------------------------------------------------
+{-# OPTIONS_GHC -F -pgmF htfpp #-}
+{-# LANGUAGE BangPatterns          #-}
+{-# LANGUAGE DoAndIfThenElse       #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE PackageImports        #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TypeSynonymInstances  #-}
+{-# LANGUAGE UndecidableInstances  #-}
+
+-- {-# OPTIONS_GHC  -fno-warn-warnings-deprecations #-}
+
+
+module Uniform.CmdLineArgs_test where
+
+import           Test.Framework
+import           Uniform.Strings hiding ((</>), (<.>), (<|>))
+import Uniform.CmdLineArgs
+
+import Control.Exception
+
+
