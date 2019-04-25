26dab9b3403347ce9eb23619167cadb7b699a8aa
andrew u frank <frank@london.local>
2019-04-25 21:46:38 +0200

    seems to work and simple to use with filepath construct systematically!

--- uniform-pathTest/tests/Uniform/PathTest_test.hs
+++ uniform-pathTest/tests/Uniform/PathTest_test.hs
@@ -28,8 +28,8 @@ import  Uniform.PathWrapper
 import qualified Path as P
 import Control.Exception
 
-s1 = Path "testFile.txt" :: Path Rel File 
-r1 = "Path \"testFile.txt\""
+s1 = Path Rel File "testFile.txt" :: Path Rel File 
+r1 = "Path Rel File \"testFile.txt\""
 test_show1 = assertEqual r1 (showT s1)
 
 test_read = assertEqual s1 (readNoteT "243" . showT $ s1 )
@@ -44,5 +44,14 @@ p3 = P.parseRelFile n1 :: Maybe (P.Path Rel File )
 s3 = path2internal s2 :: P.Path Rel File 
 p3a = s2t . P.toFilePath . fromJustNote "sdsw" $ p3 
 
-test_makeRelDir = assertEqual d4 (makeRelDir "Workspace")
-d4 = Path "Workspace/" :: Path Rel Dir 
+test_makeRelDir = assertEqual d4 f4
+f4 = makeRelDir "Workspace"
+d4 = Path Rel Dir "Workspace/" :: Path Rel Dir 
+s4 = showT d4
+test_RelDir = assertEqual d4 (readNoteT "RD" s4)
+
+test_addDir = assertEqual r6  (addDir f4  n6)
+n6 = makeRelFile . s2t $ n1 
+r6 = makeRelFile "Workspace/testFile.txt"
+
+test_filename = assertEqual n6 (getFileName r6)
