3b9102a78aec2d06d88a37a525892ae31948bbb9
andrew u frank <frank@london.local>
2020-05-26 09:02:20 +0200

    avoid error messages

--- uniform-algebras/Uniform/Pointless.hs
+++ uniform-algebras/Uniform/Pointless.hs
@@ -21,64 +21,65 @@
 {-# LANGUAGE UndecidableInstances  #-}
 
 {-# OPTIONS_GHC  -fno-warn-warnings-deprecations #-}
+{-# OPTIONS_GHC  -fno-warn-unused-matches #-}
     -- runErrorT is depreceiated but used in monads-tf
 -- {-# OPTIONS_GHC -w #-}
 
 
-module Uniform.Pointless (
-    module Uniform.Pointless
+module Uniform.Pointless
+  ( module Uniform.Pointless
     -- , module Data.Tuple.Utils
-
-        )  where
+  )
+where
 
 -- import Data.Tuple.Utils (fst3, snd3, thd3, fst4, snd4, trd4, fth4
 --             , fst5, snd5, trd5, fth5, ffh5)
 
 
-pair f (a,b) = (f a, f b)
-cross (f,g) (a,b) = (f a, g b)
-swapPair (a,b) = (b,a)
+pair f (a, b) = (f a, f b)
+cross (f, g) (a, b) = (f a, g b)
+swapPair (a, b) = (b, a)
 
-first f (a,b) = (f a, b)
-second f (a,b) = (a, f b)
+first f (a, b) = (f a, b)
+second f (a, b) = (a, f b)
 
-fst3 :: (a,b,c) -> a
-fst3 (x,y,z) = x
+fst3 :: (a, b, c) -> a
+fst3 (x, y, z) = x
 
-snd3 :: (a,b,c) -> b
-snd3 (x,y,z) = y
+snd3 :: (a, b, c) -> b
+snd3 (x, y, z) = y
 
-trd3 :: (a,b,c) -> c
-trd3 (x,y,z) = z
+trd3 :: (a, b, c) -> c
+trd3 (x, y, z) = z
 
-fst4 :: (a,b,c,d) -> a
-fst4 (x,y,z,w) = x
+fst4 :: (a, b, c, d) -> a
+fst4 (x, y, z, w) = x
 
-snd4 :: (a,b,c,d) -> b
-snd4 (x,y,z,w) = y
+snd4 :: (a, b, c, d) -> b
+snd4 (x, y, z, w) = y
 
-trd4 :: (a,b,c,d) -> c
-trd4 (x,y,z,w) = z
+trd4 :: (a, b, c, d) -> c
+trd4 (x, y, z, w) = z
 thd4 = trd4
 
-fth4 :: (a,b,c,d) -> d
-fth4 (x,y,z,w) = w
+fth4 :: (a, b, c, d) -> d
+fth4 (x, y, z, w) = w
 
-fst5 :: (a,b,c,d,e) -> a
-fst5 (x,y,z,w,u) = x
+fst5 :: (a, b, c, d, e) -> a
+fst5 (x, y, z, w, u) = x
 
-snd5 :: (a,b,c,d,e) -> b
-snd5 (x,y,z,w,u) = y
+snd5 :: (a, b, c, d, e) -> b
+snd5 (x, y, z, w, u) = y
 
-thd5 :: (a,b,c,d,e) -> c
-thd5 (x,y,z,w,u) = z
-trd5=thd5
+thd5 :: (a, b, c, d, e) -> c
+thd5 (x, y, z, w, u) = z
+trd5 = thd5
 
-fth5 :: (a,b,c,d,e) -> d
-fth5 (x,y,z,w,u) = w
+fth5 :: (a, b, c, d, e) -> d
+fth5 (x, y, z, w, u) = w
 
-ffh5 :: (a,b,c,d,e) -> e
-ffh5 (x,y,z,w,u) = u
+ffh5 :: (a, b, c, d, e) -> e
+ffh5 (x, y, z, w, u) = u
 
 
 first3 :: (a1 -> b) -> (a1, a2, a3) -> (b, a2, a3)
@@ -90,17 +91,17 @@ second3 f (a1, a2, a3) = (a1, f a2, a3)
 third3 :: (a3 -> b) -> (a1, a2, a3) -> (a1, a2, b)
 third3 f (a1, a2, a3) = (a1, a2, f a3)
 
-first4 :: (a1 -> b) -> (a1, a2, a3, a4) -> (b, a2, a3,  a4)
-first4 f (a1, a2, a3,  a4) = (f a1, a2, a3,  a4)
+first4 :: (a1 -> b) -> (a1, a2, a3, a4) -> (b, a2, a3, a4)
+first4 f (a1, a2, a3, a4) = (f a1, a2, a3, a4)
 
-second4 :: (a2 -> b) -> (a1, a2, a3,  a4) -> (a1, b, a3,  a4)
-second4 f (a1, a2, a3,  a4) = (a1, f a2, a3,  a4)
+second4 :: (a2 -> b) -> (a1, a2, a3, a4) -> (a1, b, a3, a4)
+second4 f (a1, a2, a3, a4) = (a1, f a2, a3, a4)
 
-third4 :: (a3 -> b) -> (a1, a2, a3,  a4) -> (a1, a2, b, a4)
-third4 f (a1, a2, a3,  a4) = (a1, a2, f a3,  a4)
+third4 :: (a3 -> b) -> (a1, a2, a3, a4) -> (a1, a2, b, a4)
+third4 f (a1, a2, a3, a4) = (a1, a2, f a3, a4)
 
-fourth4 :: (a4 -> b) -> (a1, a2, a3,  a4) -> (a1, a2, a3, b)
-fourth4 f (a1, a2, a3,  a4) = (a1, a2, a3, f a4)
+fourth4 :: (a4 -> b) -> (a1, a2, a3, a4) -> (a1, a2, a3, b)
+fourth4 f (a1, a2, a3, a4) = (a1, a2, a3, f a4)
 
 -- -- move TODO algebras
 -- fst3 (a,b,c) = a
