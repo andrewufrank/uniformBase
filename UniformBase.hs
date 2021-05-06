9b6d89ade5381a7a1f983722d726b0477d93c53c
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2021-05-06 11:51:16 +0200

    dropped unnecessary comments resulting from moving json out

--- UniformBase.hs
+++ UniformBase.hs
@@ -9,13 +9,7 @@ module UniformBase
     , module Uniform.Error
     , module Uniform.Strings
     , module Uniform.FileIO
-    -- , module Uniform.Json
-    -- , module Uniform.Yaml 
     , module Uniform.Time
-    -- , module Uniform.Pandoc
-    -- , module Uniform.StartApp   -- in package Error 
-    -- , module Uniform.CmdLineArgs 
-    -- , Uniform.FileIO
     , Generic
     ) where
 
@@ -24,8 +18,5 @@ import Uniform.Pointless
 import Uniform.Strings 
 import Uniform.FileIO
 import Uniform.Error
--- import Uniform.Json
--- import Uniform.Yaml hiding (encode,decode)
-    -- export qualified as Y.encode, Y.decode)
 import Uniform.Time 
 import GHC.Generics
