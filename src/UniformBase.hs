33dc2d9ddcf531b58e55dd7e90847d1ec132498d
Andrew U. Frank <frank@geoinfo.tuwien.ac.at>
2021-05-12 20:42:56 +0200

    tried hard to export Path(..) see question to SO

--- src/UniformBase.hs
+++ src/UniformBase.hs
@@ -9,9 +9,11 @@ module UniformBase
     , module Uniform.Error
     , module Uniform.Strings
     , module Uniform.FileIO
-    , module Uniform.PathShowCase
+    -- , module Uniform.PathShowCase
     , module Uniform.Time
     , Generic
+    , Read 
+    , Path (..)
     ) where
 
 import Uniform.Zero
@@ -20,7 +22,7 @@ import Uniform.Strings
 import Uniform.FileIO
 import Uniform.Error
 import Uniform.Time 
-import Uniform.PathShowCase
+-- import Uniform.PathShowCase
 import GHC.Generics
 
 -- data Aby = Aby44 Int  (Path Abs Dir)  deriving (Eq, Ord, Show, Read )
